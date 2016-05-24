8750d055b01b84e672ccdde24854aec6f96f85d8
Stan Fedora Bianca <fedorabianca@gmail.com>
2016-05-24 17:13:46 +0300

    fade in part 2

--- footer.js
+++ footer.js
@@ -1,14 +1,38 @@
 
 
 window.onload = function () {
-    var rad = document.myForm.myRadios;
+
+
+
+var rad = document.myForm.myRadios;
 for(var i = 0; i < rad.length; i++) {
     rad[i].onclick = function() {
-        alert(this.value)
+        if( this.value == "true"){
+            alert("da");
+            divArray = document.getElementsByTagName("div");
+            for( var i = 0; i<divArray.length;i++){
+                if( divArray[i].classList.contains("nofade") ){
+                    divArray[i].classList.remove("nofade");
+                    divArray[i].classList.add("fade");
+                }
+            }
+            
+        }else{
+            alert("nu");
+            divArray = document.getElementsByTagName("div");
+            for( var i = 0; i<divArray.length;i++){
+                if( divArray[i].classList.contains("fade") ){
+                    divArray[i].classList.remove("fade");
+                    divArray[i].classList.add("fade");
+                }
+            }
+        }
     };
 }
     
-    elemArray = document.getElementsByClassName("fade");
+
+    
+elemArray = document.getElementsByClassName("fade");
 opacity = 0;
 for( var i = 0; i<elemArray.length;i++)
 elemArray[i].style.opacity ="0";
