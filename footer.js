7816886c4d4bb423d663644bd4d61024726cecec
Stan Fedora Bianca <fedorabianca@gmail.com>
2016-05-24 16:46:32 +0300

    radio myFade

--- footer.js
+++ footer.js
@@ -6,7 +6,23 @@ for(var i = 0; i < rad.length; i++) {
     rad[i].onclick = function() {
         alert(this.value)
     };
-}     
+}
+    
+    elemArray = document.getElementsByClassName("fade");
+opacity = 0;
+for( var i = 0; i<elemArray.length;i++)
+elemArray[i].style.opacity ="0";
+
+var myVar = setInterval(function(){ myFadeOut() }, 100);
+
+function myFadeOut() {
+          for( var i = 0; i<elemArray.length;i++)
+      elemArray[i].style.opacity = ""+opacity;
+      opacity += 0.1;
+     if(opacity >= 1) {
+         clearInterval(myVar);
+     }
+}
 }
 
 
