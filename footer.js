a97119cec9398e171793bd6fe237f27313b23a72
Stan Fedora Bianca <fedorabianca@gmail.com>
2016-05-30 00:25:27 +0300

    add date

--- footer.js
+++ footer.js
@@ -2,6 +2,20 @@
 
 window.onload = function () {
 
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) {
+    dd='0'+dd
+} 
+if(mm<10) {
+    mm='0'+mm
+} 
+today = mm+'/'+dd+'/'+yyyy; 
+document.getElementById("time").innerHTML = today ;   
+    
+    
 
 
 var rad = document.myForm.myRadios;
